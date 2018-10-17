6d41503c32ff2d3592720a3049237a432a785b60
Ernest <erneavila@hotmail.com>
2018-10-16 22:51:09 -0500

    IMPLEMENTACION PARCIAL DE LA BIBLIOTECA, FACE 2

---
+++ js/tipueslide_content.js
@@ -0,0 +1,15 @@
+
+var tipueslide = {"pages": [
+
+     {
+        "title": "RUDIGER DORNBUSCH, STANLEY FISCHER, RICHARD STARTZ", 
+        "text":  "MACROECONOMIA",
+        "img":   "./img/84-481-4181-4.jpg", "url": ""
+      },
+      {
+        "title": "FRANK S. BUDNICK", 
+        "text":  "MATEMÁTICAS APLICADAS PARA ADMINISTRACIÓN, ECONOMÍA Y CIENCIAS SOCIALES",
+        "img":   "./img/968-422-563-6.jpg", "url": ""
+      },
+     
+]};
